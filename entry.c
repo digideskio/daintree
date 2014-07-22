819af3d043d78a780d3cdb2f564a0e24de38b298
Yuki Izumi <yuki@kivikakk.ee>
2014-07-22 18:30:29 +1000

    Add multiboot back in.

--- entry.c
+++ entry.c
@@ -4,17 +4,18 @@
 #include "cons.h"
 #include "multiboot.h"
 
-//void entry(multiboot_info_t *multiboot) {
-void entry() {
-  //if (multiboot == 0) {
+void entry(multiboot_info_t *multiboot) {
+  if (multiboot == 0) {
     puts("no multiboot info. halting.");
-    //return;
-  //}
+    return;
+  }
 
   clear();
   puts(
       "here we are\n2 hi\n3 ok\n4he\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n"
       "16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n");
+
+  asm volatile("hlt");
 }
 
 // vim: set sw=2 cc=80 et:
