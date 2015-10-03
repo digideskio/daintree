3ab94ccb959be17babab0b3d8efdccf2097c701c
Yuki Izumi <yuki@kivikakk.ee>
2015-10-03 10:51:41 +1000

    Appears to work!?

--- interrupts.c
+++ interrupts.c
@@ -59,43 +59,21 @@ void interrupts_init(void) {
     memset(&idt_entries, 0, sizeof(idt_entries));
 
 #define SET_IDT_GATE(n) set_idt_gate(0x##n, isr##n, 0x08, 0x8e)
-    SET_IDT_GATE(0);
-    SET_IDT_GATE(1);
-    SET_IDT_GATE(2);
-    SET_IDT_GATE(3);
-    SET_IDT_GATE(4);
-    SET_IDT_GATE(5);
-    SET_IDT_GATE(6);
-    SET_IDT_GATE(7);
-    SET_IDT_GATE(8);
-    SET_IDT_GATE(9);
-    SET_IDT_GATE(a);
-    SET_IDT_GATE(b);
-    SET_IDT_GATE(c);
-    SET_IDT_GATE(d);
-    SET_IDT_GATE(e);
-    SET_IDT_GATE(f);
-    SET_IDT_GATE(10);
-    SET_IDT_GATE(11);
-    SET_IDT_GATE(12);
-    SET_IDT_GATE(13);
-    SET_IDT_GATE(14);
-    SET_IDT_GATE(15);
-    SET_IDT_GATE(16);
-    SET_IDT_GATE(17);
-    SET_IDT_GATE(18);
-    SET_IDT_GATE(19);
-    SET_IDT_GATE(1a);
-    SET_IDT_GATE(1b);
-    SET_IDT_GATE(1c);
-    SET_IDT_GATE(1d);
-    SET_IDT_GATE(1e);
-    SET_IDT_GATE(1f);
+    SET_IDT_GATE(0); SET_IDT_GATE(1); SET_IDT_GATE(2); SET_IDT_GATE(3);
+    SET_IDT_GATE(4); SET_IDT_GATE(5); SET_IDT_GATE(6); SET_IDT_GATE(7);
+    SET_IDT_GATE(8); SET_IDT_GATE(9); SET_IDT_GATE(a); SET_IDT_GATE(b);
+    SET_IDT_GATE(c); SET_IDT_GATE(d); SET_IDT_GATE(e); SET_IDT_GATE(f);
+    SET_IDT_GATE(10); SET_IDT_GATE(11); SET_IDT_GATE(12); SET_IDT_GATE(13);
+    SET_IDT_GATE(14); SET_IDT_GATE(15); SET_IDT_GATE(16); SET_IDT_GATE(17);
+    SET_IDT_GATE(18); SET_IDT_GATE(19); SET_IDT_GATE(1a); SET_IDT_GATE(1b);
+    SET_IDT_GATE(1c); SET_IDT_GATE(1d); SET_IDT_GATE(1e); SET_IDT_GATE(1f);
     SET_IDT_GATE(80);
 #undef SET_IDT_GATE
 
     idt_pointer.limit = sizeof(idt_entries) - 1;
     idt_pointer.ridt = (uint32_t) idt_entries;
+
+    __asm__ __volatile__("lidt %0" : : "m" (idt_pointer));
 }
 
 void *isr_handler(struct modeswitch_registers *r) {
